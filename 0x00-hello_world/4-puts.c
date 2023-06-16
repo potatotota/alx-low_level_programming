da5fd79e82c9284fa963ef1b91970d694b609b52
potatotota <lvyw8065@gmail.com>
2023-06-15 17:32:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/**
- * main -  A program that prints the line with puts function
- * return :0 ( success)
+/*
+* main -  A program that prints the line with puts function
+ *  return :0 ( success)
  */
 int main(void)
 {
