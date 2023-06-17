67085252de58ab6820c6c11b18583562f379d36d
potatotota <lvyw8065@gmail.com>
2023-06-17 11:45:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/**
 
-Main - A program that print the line using printf function
-Return : 0 (right)
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char a;
@@ -11,10 +12,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char : %zu bytes\n", (size_t)sizeof(a));
-printf("size of a int : %zu bytes\n", (size_t)sizeof(b));
-printf("size of a long int : %zu bytes\n", (size_t)sizeof(c));
-printf("size of a long long int : %zu bytes\n", (size_t)sizeof(d));
-printf("size of a float : %zu bytes\n", (size_t)sizeof(f));
+printf("size of a char : %lu bytes\n", (size_t)sizeof(a));
+printf("size of a int : %lu bytes\n", (size_t)sizeof(b));
+printf("size of a long int : %lu bytes\n", (size_t)sizeof(c));
+printf("size of a long long int : %lu bytes\n", (size_t)sizeof(d));
+printf("size of a float : %lu bytes\n", (size_t)sizeof(f));
 return(0);
 }
